0ded65add333f588aac8e11e52639ab4df4208d9
nickhuang1121 <nickhuang1121@gmail.com>
2025-12-16 12:58:50 +0800

    update12

--- 12/12-14-C#/Program.cs
+++ 12/12-14-C#/Program.cs
@@ -5,7 +5,7 @@ void loadRom()
     var fs = new FileStream("./INVADERS", FileMode.Open);
     FileSize = (int)fs.Length;
     fs.ReadExactly(Memory, 0, FileSize);
-    Console.WriteLine($"第一個操作碼：{Memory[0]:X}{Memory[1]:X}");
-    Console.WriteLine($"容量為：{FileSize}kb");
+    Console.WriteLine($" 第一個操作碼：{Memory[0]:X}{Memory[1]:X}");
+    Console.WriteLine($" 容量為：{FileSize}kb");
 }
 loadRom();
