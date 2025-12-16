73ccabdfb60663fbc57ff3aa1583f2d91cafb469
nickhuang1121 <nickhuang1121@gmail.com>
2025-12-16 10:33:24 +0800

    fold 10

---
+++ 10/10-3-C#/Program.cs
@@ -0,0 +1,30 @@
+﻿Juice juice1 = new Juice("鮮奶茶", "紅茶", "鮮奶", "半糖", 60);
+juice1.Say();
+Juice juice2 = new Juice("葡萄多多", "葡萄汁", "養樂多", "少糖", 70);
+juice2.PriceUp();
+juice2.Say();
+public class Juice
+{
+    private string Name { get; set; }
+    private string JuiceBase { get; set; }
+    private string Mix { get; set; }
+    private string Suger { get; set; }
+    private int Price { get; set; }
+
+    public Juice(string name, string juiceBase, string mix, string suger, int price)
+    {
+        Name = name;
+        JuiceBase = juiceBase;
+        Mix = mix;
+        Suger = suger;
+        Price = price;
+    }
+    public void Say()
+    {
+        Console.WriteLine($"{Name} {Suger} {Price}元");
+    }
+    public void PriceUp()
+    {
+        Price += 5;
+    }
+}
