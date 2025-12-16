8dc07f7426362e746f19ab8cb652b9c15ece2518
nickhuang1121 <nickhuang1121@gmail.com>
2025-12-16 12:42:14 +0800

    fold11 update

---
+++ 11/11-1-C#/Program.cs
@@ -0,0 +1,4 @@
+﻿byte[] numberList = new byte[2];
+numberList[0] = 0b100000000; //此範例會無法執行，因為此行超過8bit，因此無法用byte儲存
+numberList[1] = 0b10000000;
+Console.WriteLine($"第一筆：{numberList[0]} / 第二筆：{numberList[1]}");
---
+++ 11/11-6-C#/Program.cs
@@ -0,0 +1,4 @@
+﻿byte numberA = 0b1000;
+byte numberB = 0b0001;
+byte result = (byte)(numberA | numberB);
+Console.WriteLine($" 答案：{result}");
